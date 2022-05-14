2dfeb0740d280346592370741d8795e535a928c4
bipinrai054 <bipinrai054@gmail.com>
2022-05-14 12:07:28 +0545

    data feteched from fakestore api

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,38 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import {useQuery} from 'react-query'
+
+// mui
+import {Drawer,LinearProgress,Grid,Badge} from '@material-ui/core' 
+
+// mui icons
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+
+// styles
+import {Wrapper} from './App.styles'
+
+// types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: string;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async(): Promise <CartItemType[]> => await (await fetch('https://fakestoreapi.com/products')).json()
+
+const App = () => {
+
+  const {data,isLoading,error} = useQuery<CartItemType[]>('products',getProducts)
+  console.log(data,isLoading,error)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+     Hello World!
     </div>
   )
 }
